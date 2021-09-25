a157a2068463cd627c3b14243fff8a11ab711baa
prithupratham <84316410+prithupratham@users.noreply.github.com>
2021-09-25 09:35:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("Camera");
+Webcam.attach('#camera');
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id = "Captured_image" src = "'+data_uri+'">';
+    });
+}
+
+console.log('ml5 version:',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wPEYUppYJ/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('model Loaded!');
+}
+
+function check(){
+    img = document.getElementById('Captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error){
+    console.error(error);
+    
+}
+else{
+console.log(results);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
